44d38e1f2c1abc98091a737683c994e2fbf41997
Tashrif <117228564+Tashrif2001@users.noreply.github.com>
2023-09-17 12:40:18 +0600

    Create CreateTextFileTest.java

---
+++ fileIO/CreateTextFileTest.java
@@ -0,0 +1,11 @@
+package fileIO;
+
+public class CreateTextFileTest {
+	public static void main(String[] args) {
+		CreateTextFile app = new CreateTextFile();
+		
+		app.openFile();
+		app.addRecords();
+		app.closeFile();
+	}
+}
